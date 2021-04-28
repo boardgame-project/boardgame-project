921a3c4d3c49278f4151e7f8f73b57ba17105d31
Carlos Chavez <hamandatorres@gmail.com>
2021-04-28 11:21:27 -0600

    connect routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header'
+import User from './components/User/User'
+import GameLibrary from './components/Games/GameLibrary'
 
 export default (
   <Switch>
-    <Route />
+    <Route exact path='/' component={Header} />
+    <Route  path='/user' component={User} />
+    <Route path='/game' component={GameLibrary} />
   </Switch>
 )
