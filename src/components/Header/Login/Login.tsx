3ecd7adecaf3dc1d5201b478b9192b50c582ad5e
Richard Miller <richardmmill@gmail.com>
2021-04-29 14:57:36 -0400

    work on login

--- src/components/Header/Login/Login.tsx
+++ src/components/Header/Login/Login.tsx
@@ -1,8 +1,108 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+
 
 const Login: React.FC = () => {
+  
+  interface User {
+    firstName?: string,
+    lastName?: string,
+    email: string,
+    password: string
+  }
 
-  return (<></>)
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [loginEmail, setLoginEmail] = useState<string>('')
+  const [loginPassword, setLoginPassword] = useState<string>('')
+
+  const dispatch = useDispatch();
+
+  const register = ():void => {
+    axios.post<User>('/api/auth/register', {firstName, lastName, email, password})
+    .then(res => {
+      const user = res.data
+      // console.log(res.data)
+      dispatch({type: 'UPDATE_USER', action: user})
+    })
+  };
+
+  const registerEnterPress = (event: any) => {
+    if(event.keyCode === 13) {
+      register()
+    }
+  }
+
+  const login = ():void => {
+    axios.post<User>('/api/auth/login', {loginEmail, loginPassword})
+    .then(res => {
+      const user = res.data
+      // console.log(res.data)
+      dispatch({type: 'UPDATE_USER', action: user})
+    })
+  };
+
+  const loginEnterPress = (event: any) => {
+    if(event.keyCode === 13) {
+      login()
+    }
+  }
+
+
+  return (
+    <div className='loginContainer'>
+      <form className='register'>
+        <h3>register</h3>
+        <p>first name:</p>
+        <input
+          value={firstName} 
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement>,
+            ): void => setFirstName(e.target.value)}/>
+        <p>last name:</p>
+        <input
+          value={lastName} 
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement>,
+            ): void => setLastName(e.target.value)}/>
+        <p>email:</p>
+        <input
+          value={email} 
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement>,
+            ): void => setEmail(e.target.value)}/>
+        <p>password:</p>
+        <input
+          value={password} 
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement>,
+            ): void => setPassword(e.target.value)}
+            onKeyPress={registerEnterPress}
+            />
+          <button onClick={register} type='submit'>register</button>
+      </form>
+      <form> 
+        <h3>login</h3>
+      <p>email:</p>
+        <input
+          value={loginEmail} 
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement>,
+            ): void => setLoginEmail(e.target.value)}/>
+        <p>password:</p>
+        <input
+          value={loginPassword} 
+          onChange={(
+            e: React.ChangeEvent<HTMLInputElement>,
+            ): void => setLoginPassword(e.target.value)}
+            onKeyPress={loginEnterPress}
+            />
+          <button onClick={login} type='submit'>login</button>
+      </form>
+    </div>)
 }
 
 export default Login
