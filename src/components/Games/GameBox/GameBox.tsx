5c52bb59d3bcbaf1795e0ba32c3d54e70b8ad78a
Richard Miller <richardmmill@gmail.com>
2021-04-29 16:16:22 -0400

    work on gamelibrary on 4.29 at 4:16

--- src/components/Games/GameBox/GameBox.tsx
+++ src/components/Games/GameBox/GameBox.tsx
@@ -1,7 +1,22 @@
 // import React, {useState, useEffect} from 'react';
 import React from 'react';
 
-const GameBox: React.FC = () => {
+type Game = {
+  game_id: string,
+  name: string, 
+  year_published: string, 
+  min_players: number,
+  max_players: number,
+  min_age: number,
+  mechanics: string,
+  categoties: string,
+  description: string,
+  image_url: string, 
+  thumb_url: string
+}
+
+const GameBox: React.FC<Game> = (props: Game):JSX.Element => {
+
 
   return (<></>)
 }
