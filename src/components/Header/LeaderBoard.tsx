62f9b2a6da78af9b9028edf5301ac333e9362909
Carlos A Chavez Perez <hamandatorres@gmail.com>
2021-04-30 10:04:20 -0600

    layout of leaderboard done

--- src/components/Header/LeaderBoard.tsx
+++ src/components/Header/LeaderBoard.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 
+
 type Players = {
-  user: string;
-  score: number;
-}
-
+  username: string;
+  total_plays: number;
+};
 const LeaderBoard: React.FC<Players> = (Players) => {
-  const { user, score } = Players
-
+  const { username, total_plays } = Players;
   return (
-  <div>
-    {user}
-    {score}
-  </div>
-  )
+    <div className="leaderboard">
+      <div className="boardTable">
+        {/* {boardData[scoreTimeFilter].map((d, id) => {
+          return ( */}
+        <div className="userData">
+          <div className="boardPositionWrapper">
+            <p className="userDataP">Place</p>
+          </div>
+          <div className="boardUsernameWrapper">
+            <p className="userDataP">Username {username}</p>
+          </div>
+          <div className="boardScoreWrapper">
+            <p className="userDataP">Plays {total_plays}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default LeaderBoard
