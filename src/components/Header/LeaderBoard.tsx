b7371e53e09471a191976ed872636f76616e4314
Carlos A Chavez Perez <hamandatorres@gmail.com>
2021-04-30 11:57:58 -0600

    worknig on mapping

--- src/components/Header/LeaderBoard.tsx
+++ src/components/Header/LeaderBoard.tsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 type Players = {
-  username: string;
-  total_plays: number;
+  user_id?: number;
+  total?: number;
 };
-const LeaderBoard: React.FC<Players> = (Players) => {
-  const { username, total_plays } = Players;
+const LeaderBoard: React.FC<Players> = (): JSX.Element => {
+  // const { username, total } = Players;
+
+  
+  const getLeaderboard = ():void => {
+    axios.get('/api/player/leaderboard')
+    .then(res => {
+      const leaderboardArray = res.data
+      console.log(res.data)
+      setRow(leaderboardArray)
+    }).catch(err => console.log(err))
+  }
+
+  const [row, setRow] = useState([])
+  useEffect(():void => {
+    getLeaderboard()
+  }, [])
+  
+const mappedLeaderboard = row.map((elem: Players, id: number) => {
   return (
-    <div className="leaderboard">
-      <div className="boardTable">
-        {/* {boardData[scoreTimeFilter].map((d, id) => {
-          return ( */}
-        <div className="userData">
-          <div className="boardPositionWrapper">
-            <p className="userDataP">Place</p>
-          </div>
-          <div className="boardUsernameWrapper">
-            <p className="userDataP">Username {username}</p>
-          </div>
-          <div className="boardScoreWrapper">
-            <p className="userDataP">Plays {total_plays}</p>
-          </div>
+    <div className="leaderboard" key={id}>
+    <div className="boardTable">
+      <div className="userData">
+        <div className="boardPositionWrapper">
+          <p className="userDataP"></p>
+        </div>
+        <div className="boardUsernameWrapper">
+          <p className="userDataP">{elem.user_id}</p>
+        </div>
+        <div className="boardScoreWrapper">
+          <p className="userDataP">{elem.total}</p>
         </div>
       </div>
+    </div>
+  </div> 
+  )
+
+})
+  return (
+    <div>
+      {mappedLeaderboard}
     </div>
   );
 }
