109dd03069e5a3c5beff6735f0629260e4473429
Richard Miller <richardmmill@gmail.com>
2021-05-07 14:10:31 -0400

    footer added

---
+++ src/components/Header/footer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+const Footer: React.FC = () => {
+
+  return (
+    <div className='footer'>
+        <div>
+            <h3>TopTable Games</h3>
+            <p><a href='https://www.linkedin.com/in/carlos-chavez-141926208/' rel="noreferrer" target='_blank'>Carlos Chavez</a> | <a href='https://www.linkedin.com/in/david-j-koser/' rel="noreferrer" target='_blank'>David Koser</a> | <a href='https://www.linkedin.com/in/richard-miller-wimmer/' rel="noreferrer" target='_blank'>Richard Miller Wimmer</a></p>
+        </div>
+        <div>
+            <Link to="/">
+                <div className="footerHexContainer">
+                    <div className="footerHexagon1"></div>
+                    <div className="footerHexagon2"></div>
+                    <div className="footerHexagon3"></div>
+                </div>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
