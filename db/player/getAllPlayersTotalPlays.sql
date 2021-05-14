47d53a05f56f63f84b073274b7823a9b8a666f52
David Koser <koser.david@gmail.com>
2021-05-13 22:30:22 -0500

    Place safeguard within leaderboard SQL to ensure only non-null values (already accounted for via zero initialization on game creation, but makes it easier to add test data through via postgres)

--- db/player/getAllPlayersTotalPlays.sql
+++ db/player/getAllPlayersTotalPlays.sql
@@ -8,10 +8,10 @@ FROM (
     ug.play_count play_count
   FROM
     user_games ug
-    JOIN users u ON u.user_id = ug.user_id) temp
+    JOIN users u ON u.user_id = ug.user_id
+    WHERE play_count NOTNULL) temp
 GROUP BY
   username
 ORDER BY
   total DESC
 LIMIT 5;
-
